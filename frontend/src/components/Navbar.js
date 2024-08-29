2335b615dfff252c46fc29c38960ac1f0a7c759f
Avishek-NITT <avishekmajumder2004@gmail.com>
2024-08-29 19:24:14 +0530

    feat:Introduced Routing

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const login_route = () => {
+        navigate('/login');
+    };
+
+    const home_route = () => {
+        navigate('/');
+    }
+
+
     return (  //This is a navbar
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">FALCON </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"> </span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
+                        <li className="nav-item active">
+                            <button className="btn border-0" onClick={home_route}>Home </button>
+                        </li>
+                        <li className="nav-item">
+                            <button className="btn" href="#">TEMP BTTN</button>
+                        </li>
+                    </ul>
+                 </div>
+
+                <div>
+                    <button className="btn btn-outline-success my-2 my-sm-0 mr-sm-2" type="submit" onClick={login_route}>Log in</button>
+                    <button className="btn btn-outline-success my-2 my-sm-0 mr-sm-2" type="submit">Sign up</button>
+                </div>
+
+            </nav>
+                    
+
+
+                    
+                    {/* <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Dropdown
                         </a>
@@ -29,17 +54,17 @@ function Navbar() {
                         <div className="dropdown-divider"></div>
                         <a className="dropdown-item" href="#">Something else here</a>
                         </div>
-                    </li>
-                    <li className="nav-item">
+                    </li> */}
+                    {/* <li className="nav-item">
                         <a className="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                    </ul>
-                    <form className="form-inline my-2 my-lg-0">
+                    </li> */}
+
+
+
+                    {/* <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
+                    </form> */}
         </>
     );
 }
--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navbar from '../components/Navbar';
+
 
 // Define the component using PascalCase
 function Login() {
     return (
         <>
+            <Navbar/>
+
             This is the login page
         </>
     );
